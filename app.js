bb1858d7c17cd1437b1cc0d4c651f20153c19178
FaizanKhanDev <faizan480khan@gmail.com>
2023-05-14 08:47:43 +0500

    initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import web from "./routes/web.js";
+
+const app = express()
+const port = process.env.PORT || "3000"
+
+app.set('views engine', "ejs")
+app.use('/', web)
+
+
+app.listen(port, () => {
+    console.log(`server listening http://localhost:${port}`)
+})
